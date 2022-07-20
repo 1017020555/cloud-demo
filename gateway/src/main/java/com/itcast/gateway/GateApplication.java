5f63039bf5d2542b20e41872a4ba79701c8a30c9
“lichao” <1017020555@qq.com>
2022-07-20 14:37:20 +0800

    Gateway路由相关配置

---
+++ gateway/src/main/java/com/itcast/gateway/GateApplication.java
@@ -0,0 +1,14 @@
+package com.itcast.gateway;
+
+import com.sun.glass.ui.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateApplication.class,args);
+    }
+
+}
